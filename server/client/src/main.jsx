a8a2b292e02beca13b666e4129d87e51d13f4966
Robert Ramos <robert.ramos07281021@gmail.com>
2024-10-27 09:44:16 +0800

    9:44

--- server/client/src/main.jsx
+++ server/client/src/main.jsx
@@ -1,14 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import store from './redux/store.js'
-import { createRoutesFromElements, RouterProvider, Route } from 'react-router'
-import { createBrowserRouter } from 'react-router-dom'
-import Login from './pages/Login.jsx'
-import AdminRoute from './pages/adminRoute/AdminRoute.jsx'
-import HRDashboard from './pages/adminRoute/HRDashboard.jsx'
-import Dashboard from './pages/Dashboard.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
+import { createRoutesFromElements, RouterProvider, Route } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
+import Login from './pages/Login.jsx';
+import AdminRoute from './pages/adminRoute/AdminRoute.jsx';
+import HRDashboard from './pages/adminRoute/HRDashboard.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -18,7 +18,6 @@ const router = createBrowserRouter(
       <Route path='' element={<AdminRoute/>}>
         <Route path='/dashboard' element={<HRDashboard/>} />
       </Route>
-
     </Route>
   )
 )
